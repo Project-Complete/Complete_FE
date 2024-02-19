4852f7e100fc94cda403f40c49117b81b7488cd5
seonnn <ssni0105@gmail.com>
2024-02-19 22:53:21 +0900

    feat: reviewWriteForm 컴포넌트 추가

---
+++ apps/web/app/drink/[detail]/review/write/(components)/ReviewWriteForm.tsx
@@ -0,0 +1,164 @@
+'use client';
+import React, { useState } from 'react';
+import SectionHeader from './SectionHeader';
+import classes from './ReviewWriteForm.module.scss';
+import Image from 'next/image';
+import HelpMessageButton from './HelpMessageButton';
+import { Rating } from '@mantine/core';
+import SituationButton from './SituationButton';
+import FoodButton from './FoodButton';
+
+export interface Situation {
+  alone: boolean;
+  friend: boolean;
+  partner: boolean;
+  business: boolean;
+  adult: boolean;
+}
+
+export type SituationItemsValueType = keyof Situation;
+
+export type SituationItemsLabelType =
+  | '나 혼자'
+  | '친구'
+  | '연인'
+  | '비즈니스'
+  | '웃어른';
+
+interface SituationItems {
+  value: SituationItemsValueType;
+  label: SituationItemsLabelType;
+}
+
+const situationItems: SituationItems[] = [
+  { value: 'alone', label: '나 혼자' },
+  { value: 'friend', label: '친구' },
+  { value: 'partner', label: '연인' },
+  { value: 'business', label: '비즈니스' },
+  { value: 'adult', label: '웃어른' },
+];
+
+const flavorItems = [
+  { value: 1, label: '과일' },
+  { value: 2, label: '견과류' },
+  { value: 3, label: '곡물' },
+  { value: 4, label: '꽃' },
+  { value: 5, label: '달콤' },
+  { value: 6, label: '발효' },
+  { value: 7, label: '식물' },
+  { value: 8, label: '유제품' },
+  { value: 9, label: '향신료' },
+  { value: 10, label: '기타' },
+];
+
+const foodItems = [
+  { value: 1, label: '가공육' },
+  { value: 2, label: '견과류' },
+  { value: 3, label: '과일' },
+  { value: 4, label: '마른 안주' },
+  { value: 5, label: '매운 음식' },
+  { value: 6, label: '스낵' },
+  { value: 7, label: '양식' },
+  { value: 8, label: '육류' },
+  { value: 9, label: '치즈' },
+  { value: 10, label: '탕' },
+  { value: 11, label: '튀김' },
+  { value: 12, label: '해산물' },
+];
+
+const ReviewWriteForm = () => {
+  const [rate, setRate] = useState(0);
+  const [situation, setSituation] = useState<Situation>({
+    alone: false,
+    friend: false,
+    partner: false,
+    business: false,
+    adult: false,
+  });
+  const [food, setFood] = useState<Set<number>>(new Set());
+
+  return (
+    <form className={classes['form']}>
+      <section>
+        <SectionHeader title='인증샷' dot={false} />
+        <button className={classes['image-attach']}>
+          사진 첨부하기
+          <Image
+            src={'/icons/카메라.svg'}
+            alt='camera'
+            height={24}
+            width={24}
+          />
+        </button>
+        <HelpMessageButton />
+      </section>
+
+      <section>
+        <SectionHeader title='칠러님의 별점 및 리뷰' dot={false} />
+        <Rating
+          size={40}
+          value={rate}
+          onChange={setRate}
+          fullSymbol={
+            <Image src={'/icons/별_1.svg'} alt='1' width={40} height={40} />
+          }
+          emptySymbol={
+            <Image src={'/icons/별_0.svg'} alt='0' width={40} height={40} />
+          }
+        />
+        <HelpMessageButton />
+        <textarea className={classes['textarea']} />
+        <HelpMessageButton />
+      </section>
+
+      <div className={classes['situation-section-wrapper']}>
+        <SectionHeader title='누구랑 마시면 좋을까요?' />
+        <div>
+          {situationItems.map(item => (
+            <SituationButton
+              key={item.value}
+              label={item.label}
+              value={item.value}
+              isSelected={situation[item.value]}
+              situation={situation}
+              setSituation={setSituation}
+            />
+          ))}
+        </div>
+      </div>
+
+      <SectionHeader title='어떤 맛이 느껴지나요?' />
+
+      <div className={classes['flavor-section-wrapper']}>
+        <SectionHeader title='어떤 향이 느껴지나요?' />
+        <div>
+          {flavorItems.map(item => (
+            // chip button으로 변경
+            <button className={classes['chip-button']} type='button'>
+              {item.label}
+            </button>
+          ))}
+        </div>
+        <HelpMessageButton />
+      </div>
+
+      <div className={classes['food-section-wrapper']}>
+        <SectionHeader title='어떤 안주랑 어울리나요?' />
+        <div>
+          {foodItems.map(item => (
+            <FoodButton
+              key={item.value}
+              label={item.label}
+              value={item.value}
+              isSelected={food.has(item.value)}
+              food={food}
+              setFood={setFood}
+            />
+          ))}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ReviewWriteForm;
