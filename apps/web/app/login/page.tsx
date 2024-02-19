bf586c5fca5797f36221b3a31ac01bd41e4c4427
JSC <gbs04087@gmail.com>
2024-02-19 23:41:50 +0900

    feat: add login with middleware setting

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,23 +1,17 @@
-// 'use client'
-import { Flex, Container, Button, Box } from '@mantine/core';
-import containerCss from './container.module.scss'
-import Image from 'next/image'
+import { Flex, Box } from '@mantine/core';
+import containerCss from './container.module.scss';
 import LoginButton from '@/components/login/LoginButton';
 
-
-
-
-
 export default function Page(): JSX.Element {
-    return (
-        <>
-            <Flex classNames={containerCss}>
-                <Box w={600} h={200}>
-                    asdf
-                </Box>
-                <LoginButton />
-                <Flex>궁금하신 사항 있으신가요?</Flex>
-            </Flex>
-        </>
-    );
+  return (
+    <>
+      <Flex classNames={containerCss}>
+        <Box w={600} h={200}>
+          asdf
+        </Box>
+        <LoginButton />
+        <Flex>궁금하신 사항 있으신가요?</Flex>
+      </Flex>
+    </>
+  );
 }
--- apps/web/components/login/LoginButton.tsx
+++ apps/web/components/login/LoginButton.tsx
@@ -1,27 +1,56 @@
-'use client'
+'use client';
 
 import { Flex, Container, Button, Box } from '@mantine/core';
-import Image from 'next/image'
-import loginButtonCss from './loginButton.module.scss'
-import { useRouter } from 'next/navigation'
+import Image from 'next/image';
+import loginButtonCss from './loginButton.module.scss';
+import { useRouter } from 'next/navigation';
 
-const loginTypeList = [
-    { path: `${process.env.NEXT_PUBLIC_BACKEND_URL}oauth2/authorization/naver`, content: '네이버 로그인', src: '/icons/naver-icon.svg', bg: '#03CF5D', c: "#FFFFFF" },
-    { path: `${process.env.NEXT_PUBLIC_BACKEND_URL}oauth2/authorization/kakao`, content: '카카오 로그인', src: '/icons/kakao-icon.svg', bg: '#FEE500', c: "#000000" },
-    { path: `${process.env.NEXT_PUBLIC_BACKEND_URL}oauth2/authorization/google`, content: '구글 로그인', src: '/icons/google-icon.svg', bg: '#FFF', c: "#000000" }
-]
+type LoginButtonPropsType = {};
+const LoginButton = ({}: LoginButtonPropsType) => {
+  const loginTypeList = [
+    {
+      path: `/oauth2/authorization/naver`,
+      content: '네이버 로그인',
+      src: '/icons/naver-icon.svg',
+      bg: '#03CF5D',
+      c: '#FFFFFF',
+    },
+    {
+      path: `/oauth2/authorization/kakao`,
+      content: '카카오 로그인',
+      src: '/icons/kakao-icon.svg',
+      bg: '#FEE500',
+      c: '#000000',
+    },
+    {
+      path: `/oauth2/authorization/google`,
+      content: '구글 로그인',
+      src: '/icons/google-icon.svg',
+      bg: '#FFF',
+      c: '#000000',
+    },
+  ];
 
-type LoginButtonPropsType = {}
-const LoginButton = ({ }: LoginButtonPropsType) => {
-
-    const router = useRouter();
-
-    return <Flex direction={'column'} gap={16} justify={'center'} align={'center'}>
-        {loginTypeList.map(({ content, src, bg, c, path }) => {
-            return <Button onClick={() => router.push(path)} classNames={loginButtonCss} bg={bg} c={c} leftSection={<Image src={src} width={40} height={40} alt={'content'} />} >
-                {content}
-            </Button>
-        })}
+  return (
+    <Flex direction={'column'} gap={16} justify={'center'} align={'center'}>
+      {loginTypeList.map(({ content, src, bg, c, path }, index) => {
+        return (
+          <Button
+            component='a'
+            key={index}
+            href={path}
+            classNames={loginButtonCss}
+            bg={bg}
+            c={c}
+            leftSection={
+              <Image src={src} width={40} height={40} alt={'content'} />
+            }
+          >
+            {content}
+          </Button>
+        );
+      })}
     </Flex>
-}
+  );
+};
 export default LoginButton;
