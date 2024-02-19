601ae3bc2a14711c007b9f0228f261a76ded7e62
seonnn <ssni0105@gmail.com>
2024-02-19 22:55:59 +0900

    feat: helpMessageButton 컴포넌트 추가

---
+++ apps/web/app/drink/[detail]/review/write/(components)/HelpMessageButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './HelpMessageButton.module.scss';
+
+const HelpMessageButton = () => {
+  return <button className={classes['help-message']}>Help Message</button>;
+};
+
+export default HelpMessageButton;
