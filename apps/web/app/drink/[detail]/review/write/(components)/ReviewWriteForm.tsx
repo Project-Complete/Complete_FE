c03b01b35c16680dd49a125a6458cc81e7520d61
seonnn <ssni0105@gmail.com>
2024-02-22 02:37:24 +0900

    feat: mantine form 적용 및 taste 평가 기능 추가

--- apps/web/app/drink/[detail]/review/write/(components)/ReviewWriteForm.tsx
+++ apps/web/app/drink/[detail]/review/write/(components)/ReviewWriteForm.tsx
@@ -7,6 +7,8 @@ import HelpMessageButton from './HelpMessageButton';
 import { Rating } from '@mantine/core';
 import SituationButton from './SituationButton';
 import FoodButton from './FoodButton';
+import { ReviewFormProvider, useReviewForm } from './form-context';
+import TasteInput from './TasteInput';
 
 export interface Situation {
   alone: boolean;
@@ -67,97 +69,136 @@ const foodItems = [
 ];
 
 const ReviewWriteForm = () => {
-  const [rate, setRate] = useState(0);
-  const [situation, setSituation] = useState<Situation>({
-    alone: false,
-    friend: false,
-    partner: false,
-    business: false,
-    adult: false,
+  const form = useReviewForm({
+    initialValues: {
+      content: '',
+      situation: {
+        alone: false,
+        friend: false,
+        partner: false,
+        business: false,
+        adult: false,
+      },
+      taste: {
+        sweet: 0,
+        sour: 0,
+        bitter: 0,
+        body: 0,
+        refresh: 0,
+      },
+      flavors: Array.from({ length: foodItems.length }, () => false),
+      foods: Array.from({ length: foodItems.length }, () => false),
+    },
+    validate: {},
   });
-  const [food, setFood] = useState<Set<number>>(new Set());
+  const [rate, setRate] = useState(0);
 
   return (
-    <form className={classes['form']}>
-      <section>
-        <SectionHeader title='인증샷' dot={false} />
-        <button className={classes['image-attach']}>
-          사진 첨부하기
-          <Image
-            src={'/icons/카메라.svg'}
-            alt='camera'
-            height={24}
-            width={24}
+    <ReviewFormProvider form={form}>
+      <form
+        className={classes['form']}
+        onSubmit={form.onSubmit(values => console.log(values))}
+      >
+        <button type={'submit'}>버튼</button>
+        <section>
+          <SectionHeader title='인증샷' dot={false} />
+          <button className={classes['image-attach']}>
+            사진 첨부하기
+            <Image
+              src={'/icons/카메라.svg'}
+              alt='camera'
+              height={24}
+              width={24}
+            />
+          </button>
+          <HelpMessageButton />
+        </section>
+
+        <section>
+          <SectionHeader title='칠러님의 별점 및 리뷰' dot={false} />
+          <Rating
+            size={40}
+            value={rate}
+            onChange={setRate}
+            fullSymbol={
+              <Image src={'/icons/별_1.svg'} alt='1' width={40} height={40} />
+            }
+            emptySymbol={
+              <Image src={'/icons/별_0.svg'} alt='0' width={40} height={40} />
+            }
           />
-        </button>
-        <HelpMessageButton />
-      </section>
+          <HelpMessageButton />
+          <textarea
+            className={classes['textarea']}
+            {...form.getInputProps('content')}
+          />
+          <HelpMessageButton />
+        </section>
 
-      <section>
-        <SectionHeader title='칠러님의 별점 및 리뷰' dot={false} />
-        <Rating
-          size={40}
-          value={rate}
-          onChange={setRate}
-          fullSymbol={
-            <Image src={'/icons/별_1.svg'} alt='1' width={40} height={40} />
-          }
-          emptySymbol={
-            <Image src={'/icons/별_0.svg'} alt='0' width={40} height={40} />
-          }
-        />
-        <HelpMessageButton />
-        <textarea className={classes['textarea']} />
-        <HelpMessageButton />
-      </section>
-
-      <div className={classes['situation-section-wrapper']}>
-        <SectionHeader title='누구랑 마시면 좋을까요?' />
-        <div>
-          {situationItems.map(item => (
-            <SituationButton
-              key={item.value}
-              label={item.label}
-              value={item.value}
-              isSelected={situation[item.value]}
-              situation={situation}
-              setSituation={setSituation}
-            />
-          ))}
+        <div className={classes['situation-section-wrapper']}>
+          <SectionHeader title='누구랑 마시면 좋을까요?' />
+          <div>
+            {situationItems.map(item => (
+              <SituationButton
+                key={item.value}
+                label={item.label}
+                value={item.value}
+                isSelected={form.values.situation[item.value]}
+              />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <SectionHeader title='어떤 맛이 느껴지나요?' />
-
-      <div className={classes['flavor-section-wrapper']}>
-        <SectionHeader title='어떤 향이 느껴지나요?' />
-        <div>
-          {flavorItems.map(item => (
-            // chip button으로 변경
-            <button className={classes['chip-button']} type='button'>
-              {item.label}
-            </button>
-          ))}
+        <SectionHeader title='어떤 맛이 느껴지나요?' />
+        <div className={classes['taste-section-wrapper']}>
+          <div className={classes['taste-guide']}>
+            <span>약함</span>
+            <span>강함</span>
+          </div>
+          <div className={classes['taste-inputs-wrapper']}>
+            <TasteInput taste='sweet' />
+            <TasteInput taste='sour' />
+            <TasteInput taste='bitter' />
+            <TasteInput taste='body' />
+            <TasteInput taste='refresh' />
+          </div>
         </div>
-        <HelpMessageButton />
-      </div>
 
-      <div className={classes['food-section-wrapper']}>
-        <SectionHeader title='어떤 안주랑 어울리나요?' />
-        <div>
-          {foodItems.map(item => (
-            <FoodButton
-              key={item.value}
-              label={item.label}
-              value={item.value}
-              isSelected={food.has(item.value)}
-              food={food}
-              setFood={setFood}
-            />
-          ))}
+        <div className={classes['flavor-section-wrapper']}>
+          <SectionHeader title='어떤 향이 느껴지나요?' />
+          <div>
+            {flavorItems.map((item, idx) => (
+              <label
+                key={item.value}
+                className={`${classes['flavor-chip']} ${form.values.flavors[idx] && classes['selected']}`}
+              >
+                <input
+                  type='checkbox'
+                  value={item.value}
+                  {...form.getInputProps(`flavors.${item.value - 1}`)}
+                />
+                {item.label}
+              </label>
+            ))}
+          </div>
+          <HelpMessageButton />
         </div>
-      </div>
-    </form>
+
+        <div className={classes['food-section-wrapper']}>
+          <SectionHeader title='어떤 안주랑 어울리나요?' />
+          <div>
+            {foodItems.map((item, idx) => (
+              <FoodButton
+                key={item.value}
+                label={item.label}
+                value={item.value}
+                isSelected={form.values.foods[idx]}
+              />
+            ))}
+          </div>
+        </div>
+      </form>
+    </ReviewFormProvider>
   );
 };
 
