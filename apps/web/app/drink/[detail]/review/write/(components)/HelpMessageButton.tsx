94287ed4a4a37c092cf79e4ca05b0c85d1df505e
seonnn <ssni0105@gmail.com>
2024-02-21 23:46:00 +0900

    feat: 버튼 타입 적용

--- apps/web/app/drink/[detail]/review/write/(components)/HelpMessageButton.tsx
+++ apps/web/app/drink/[detail]/review/write/(components)/HelpMessageButton.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './HelpMessageButton.module.scss';
 
 const HelpMessageButton = () => {
-  return <button className={classes['help-message']}>Help Message</button>;
+  return (
+    <button className={classes['help-message']} type='button'>
+      Help Message
+    </button>
+  );
 };
 
 export default HelpMessageButton;
