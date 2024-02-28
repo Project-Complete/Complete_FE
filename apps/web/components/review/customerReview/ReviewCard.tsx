e6047c1dd96cff5abe0524561296c39711416bf4
TypingOK <wjdwls717@gmail.com>
2024-02-28 22:59:07 +0900

    Feat: 리뷰 디테일

--- apps/web/components/review/customerReview/ReviewCard.tsx
+++ apps/web/components/review/customerReview/ReviewCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useReviewDetailQuery } from '@/hooks/queries/useReviewQuery';
+import { Box, Flex } from '@mantine/core';
 import {
   ModalContent,
   ModalRoot,
@@ -7,6 +8,7 @@ import {
   ModalHeader,
   ModalBody,
 } from '@team-complete/complete-ui';
+import Image from 'next/image';
 import { Dispatch, SetStateAction } from 'react';
 
 const CustomerReviewCard = ({
@@ -15,22 +17,38 @@ const CustomerReviewCard = ({
   reviewId,
 }: {
   modalOpen: boolean;
-  modalHandler: Dispatch<SetStateAction<boolean>>;
+  modalHandler: () => void;
   reviewId: number;
 }) => {
   const { data } = useReviewDetailQuery({ reviewId });
   console.log(data);
-  return (
-    <ModalRoot opened={modalOpen} onClose={modalHandler}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader></ModalHeader>
-        <ModalBody>
-          <div>안녕하세요!</div>
-        </ModalBody>
-      </ModalContent>
-    </ModalRoot>
-  );
+  if (data) {
+    return (
+      <ModalRoot opened={modalOpen} onClose={modalHandler}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Flex>
+              <Box>
+                <Image
+                  src={data.image_url}
+                  alt='리뷰 사진'
+                  width={80}
+                  height={80}
+                  sizes={'width:80px; height:80px'}
+                />
+              </Box>
+            </Flex>
+          </ModalHeader>
+          <ModalBody>
+            <div>안녕하세요!</div>
+          </ModalBody>
+        </ModalContent>
+      </ModalRoot>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default CustomerReviewCard;
