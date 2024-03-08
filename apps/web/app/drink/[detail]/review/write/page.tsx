19067b0f0f8c94b4bbd64b45a0d487bf9cfd9e67
seonnn <ssni0105@gmail.com>
2024-03-08 23:52:05 +0900

    feat: params 가져오기 기능 추가(drink id)

--- apps/web/app/drink/[detail]/review/write/page.tsx
+++ apps/web/app/drink/[detail]/review/write/page.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './page.module.scss';
 import ReviewWriteForm from './(components)/ReviewWriteForm';
 
-const ReviewWritePage = () => {
+const ReviewWritePage = ({
+  params: { detail },
+}: {
+  params: { detail: string };
+}) => {
   return (
     <div className={classes['review-wrapper']}>
       <h1 className={classes['page-title']}>술 리뷰 작성하기</h1>
       <h2>유럽식 레몬 라들러 스타일 기반 과일 맥주</h2>
-      <ReviewWriteForm />
+      <ReviewWriteForm drinkId={detail} />
     </div>
   );
 };
